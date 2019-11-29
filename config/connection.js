3ec3830a3b2101065f5096ef76249aaad5b1786b
Wismy Seide <wismyseide@outlook.com>
2019-11-29 18:08:52 -0500

    Before Heroku Deployment

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ if (process.env.JAWSDB_URL) {
         host: "localhost",
         port: 3306,
         user: "root",
-        password: Ws07083ws,
+        password: "Ws07083ws",
         database: "burgers_db"
     });
 }
--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,3 @@
--- ### Schema
 DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
